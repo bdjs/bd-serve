b2022ef38a9166b7de3615a02b9d8bc70230ea46
Desen Meng <mengdesen09@qq.com>
2015-07-29 15:14:48 +0800

    add bd-views

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 'use strict';
 
-var serve = require('bd-static');
 var path = require('path');
+var serve = require('bd-static');
+var views = require('bd-views');
+var assign = require('object-assign');
 
-module.exports = function (app) {
-  var keys = Object.keys(app.apps);
-  keys.map(function (key) {
-    app.use(serve(path.join(app.apps[key], 'public'), key))
+module.exports = function(app) {
+  var apps = Object.keys(app.apps);
+  apps.map(function(key) {
+    app.use(serve(path.join(app.apps[key], 'public'), key));
+    if (!app.configs.views) {
+      return;
+    }
+    var config = app.configs.views[key];
+    if (!config) {
+      return;
+    }
+    config = assign(config, {
+      key: key,
+      path: path.join(app.apps[key], 'views')
+    });
+    app.use(views(config));
   })
 };
